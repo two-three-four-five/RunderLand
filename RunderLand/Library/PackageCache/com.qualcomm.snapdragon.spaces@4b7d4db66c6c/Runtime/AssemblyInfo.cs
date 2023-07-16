ca9701ecb6d682e3adb1ffc1a4fbf3ebd4db2a17
Jukim2 <junseojskim@gmail.com>
2023-07-16 15:42:06 +0900

    Delete Library

--- RunderLand/Library/PackageCache/com.qualcomm.snapdragon.spaces@4b7d4db66c6c/Runtime/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿/******************************************************************************
- * File: AssemblyInfo.cs
- * Copyright (c) 2023 Qualcomm Technologies, Inc. and/or its subsidiaries. All rights reserved.
- *
- * Confidential and Proprietary - Qualcomm Technologies, Inc.
- *
- ******************************************************************************/
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Snapdragon.Spaces.Editor")]
